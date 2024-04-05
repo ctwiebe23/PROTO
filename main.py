978e345229f9f0ab2aaaeb92417bc7dcb4cbd91a
Mozzie <mozzie@mozzie.local>
2024-04-05 15:23:55 -0500

    created pause_until and changed formatting

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import make
 
-button1 = make.button( 20 )
-button2 = make.button( 21 )
+# on_button  = make.button( 1 )
+# off_button = make.button( 2 )
 
-motor1 = make.motor(  8,  9 )
-motor2 = make.motor( 10, 11 )
+# left_motor  = make.motor( 1 )
+# right_motor = make.motor( 2 )
 
-# servo1 = make.servo( 15 )
+# # servo1 = make.servo( 4 )
 
-while True:
-    if button1.is_pressed():
-        # servo1.spin( 0 )
-        motor1.spin(  0.5 )
-        motor2.spin( -0.5 )
+# while True:
+#     if on_button.is_pressed():
+#         # servo1.spin( 0 )
+#         left_motor.spin( 50 )
+#         right_motor.spin( -50 )
 
-    if button2.is_pressed():
-        # servo1.spin( 180 )
-        motor1.spin( 0 )
-        motor2.spin( 0 )
+#     if off_button.is_pressed():
+#         # servo1.spin( 180 )
+#         left_motor.spin( 0 )
+#         right_motor.spin( 0 )
 
-    make.pause( 0.05 )
+#     make.pause( 0.05 )
+
+on_button  = make.button( 1 )
+off_button = make.button( 2 )
+
+motor = make.motor( 1 )
+
+make.pause_until(on_button.is_pressed)
+motor.spin( 50 )
+
+make.pause_until(off_button.is_pressed)
+motor.spin( 0 )
+
+make.pause( 3 )
+
+while not off_button.is_pressed():
+    # must wait until the cycle ends to check condition
+    motor.spin( 25, 3 )
+    make.pause( 3 )
+    motor.spin( -25, 3 )
+    make.pause( 3 )
--- make.py
+++ make.py
@@ -12,31 +12,29 @@ carstonwiebe17@gmail.com
 CIRCUITPYTHON code to test the Maker PI RP2040 with servos and dc motors
 """
 
-D_CYCLE = 2 ** 15  # duty cycle
-FRQ     = 50       # frequency
+CYCLE = 2 ** 15  # duty cycle
+FRQ   = 50       # frequency
 
-button_pin = {
-    20: board.GP20,
-    21: board.GP21,
+BUTTON_PIN = {
+    1: board.GP20,
+    2: board.GP21,
 }
-servo_pin = {
-    12: board.GP12,
-    13: board.GP13,
-    14: board.GP14,
-    15: board.GP15,
+SERVO_PIN = {
+    1: board.GP12,
+    2: board.GP13,
+    3: board.GP14,
+    4: board.GP15,
 }
-dc_pin = {
-    8:  board.GP8,
-    9:  board.GP9,
-    10: board.GP10,
-    11: board.GP11,
+DC_PIN = {
+    1: (board.GP8,  board.GP9 ),
+    2: (board.GP10, board.GP11),
 }
 
 class button:
     "An object representing a button"
 
     def __init__( self, pin: int ):
-        self.io = digitalio.DigitalInOut( button_pin[pin] )
+        self.io = digitalio.DigitalInOut( BUTTON_PIN[pin] )
 
     def is_pressed( self ) -> bool:
         "Returns true if the button is pressed, false otherwise"
@@ -46,11 +44,7 @@ class servo:
     "An object representing a servo"
 
     def __init__( self, pin: int ):
-        pwm = pwmio.PWMOut(
-            servo_pin[pin],
-            duty_cycle = D_CYCLE,
-            frequency  = FRQ
-        )
+        pwm = pwmio.PWMOut( SERVO_PIN[pin], duty_cycle = CYCLE, frequency = FRQ )
         self.io = servo.Servo( pwm )
 
     def spin( self, speed: float ) -> None:
@@ -61,17 +55,18 @@ class servo:
 class motor:
     "An object representing a DC motor"
 
-    def __init__( self, pin1: int, pin2: int ):
-        forward  = pwmio.PWMOut( dc_pin[pin1], frequency = FRQ )
-        backward = pwmio.PWMOut( dc_pin[pin2], frequency = FRQ )
-        self.io  = motor.DCMotor( forward, backward )
+    def __init__( self, pinset: int ):
+        forward    = pwmio.PWMOut( DC_PIN[pinset][0], frequency = FRQ )
+        backward   = pwmio.PWMOut( DC_PIN[pinset][1], frequency = FRQ )
+        self.io    = motor.DCMotor( forward, backward )
 
     def spin( self, speed: float, seconds: float = None ) -> None:
         """
         Spin the motor at the given speed for the given time period; if no
         period is given then it spins until stopped
         """
-        self.io.throttle = speed
+        speed = 100 if speed > 100 else -100 if speed < -100 else speed
+        self.io.throttle = speed / 100
         if seconds != None:
             pause( seconds )
             self.io.spin( 0 )
@@ -80,10 +75,12 @@ def pause( seconds: float ) -> None:
     "Wait the given number of seconds before moving on"
     time.sleep( seconds )
 
-def loop(
-        code:      Callable[[], None],
-        condition: Callable[[], bool] = lambda : False
-    ) -> None:
+def pause_until( condition: Callable[[], bool] ) -> None:
+    "Wait until the given condition is satisfied"
+    while not condition():
+        pause( 0.05 )
+
+def loop( code: Callable[[], None], condition: Callable[[], bool] = lambda : False ) -> None:
     """
     Loops the given code indefinitely or until the given termination condition
     is met
