6855455f61b932d68b56db184349374ae3e09283
Mozzie <mozzie@mozzie.local>
2024-04-05 15:52:16 -0500

    formatting

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
 import make
 
-# on_button  = make.button( 1 )
-# off_button = make.button( 2 )
-
-# left_motor  = make.motor( 1 )
-# right_motor = make.motor( 2 )
-
-# # servo1 = make.servo( 4 )
-
-# while True:
-#     if on_button.is_pressed():
-#         # servo1.spin( 0 )
-#         left_motor.spin( 50 )
-#         right_motor.spin( -50 )
-
-#     if off_button.is_pressed():
-#         # servo1.spin( 180 )
-#         left_motor.spin( 0 )
-#         right_motor.spin( 0 )
-
-#     make.pause( 0.05 )
-
 on_button  = make.button( 1 )
 off_button = make.button( 2 )
 
