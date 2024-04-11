0281ed7bdd6a9a7ac458b596b0975ba1d6c5caa8
C Wiebe <carstonwiebe17@gmail.com>
2024-04-10 20:33:40 -0500

    testing git log

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import make
 on_button  = make.button( 1 )
 off_button = make.button( 2 )
 
-motor = make.motor( 7 )
+motor = make.motor( 8 )
 
 make.pause_until( on_button.is_pressed )
 motor.spin( 50 )
