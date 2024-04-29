2dc71f21153d41010e2bf95db08329963327a333
C Wiebe <carstonwiebe17@gmail.com>
2024-04-29 18:03:29 -0500

    added MaKeTeX.sh

---
+++ doc/techdoc/MaKeTeX.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# builds our techdoc
+
+NAME='techdoc'
+
+# if .TEX file does not exist, throw an error
+if ! [ -f "$NAME.tex" ]
+then
+  echo "ERROR: could not locate .TEX"
+  exit 1
+fi
+
+# generate the .PDF
+pdflatex "$NAME.tex"
+pdflatex "$NAME.tex"
+
+# marked for death
+MARKED=(
+  'aux'
+  'log'
+  'out'
+  'toc'
+)
+
+# if marked file exists, remove it
+for EXT in "${MARKED[@]}"
+do
+  if [ -f "$NAME.$EXT" ]
+  then
+    rm "$NAME.$EXT"
+  fi
+done
