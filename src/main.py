dc83fedacd8cb83c81eda40bc2bd432d1f09455e
C Wiebe <carstonwiebe17@gmail.com>
2024-05-01 21:59:55 -0500

    formatting

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import make
 
 
-onbutton  = make.button(1)
-offbutton = make.button(2)
-servo     = make.smallmotor(3)
+onbutton  = make.button( 1 )
+offbutton = make.button( 2 )
+servo     = make.smallmotor( 3 )
 
 
-while not (onbutton.pressed() and offbutton.pressed()):
+while not ( onbutton.pressed() and offbutton.pressed() ):
     
     if onbutton.pressed():
-        servo.spin(100)
+        servo.spin( 100 )
 
     if offbutton.pressed():
-        servo.spin(-100)
+        servo.spin( -100 )
         
     make.pause()
 
