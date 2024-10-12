00f2c44d678461b7db472e485d21ba8c330f5809
C Wiebe <carstonwiebe17@gmail.com>
2024-10-12 01:00:25 -0500

    tested generated code

--- src/main.py
+++ src/main.py
@@ -1,33 +1,55 @@
-import lib.make as make
-
-stop    = make.button(2)
-left    = make.largemotor(7)
-right   = make.largemotor(8)
-robot   = make.drivetrain(left, right)
-arm     = make.smallmotor(6)
-
-robot.drive(50, 2)
-robot.turn(75, 1)
-robot.drive(50, 2)
-
-arm.spin(100, 1.7)
-arm.spin(-100, 1.7)
-
-robot.turn(25)
-make.wait_until(stop.pressed)
-
-arm.spin(50)
-robot.drive(50)
-make.wait(10)
-arm.stop()
-
-arm.spin(50, 2)
-
-# while not stop.pressed():
-#     arm.spin(50, 0.05)
-    
-# arm.spin(50)
+# import lib.make as make
+#
+# stop    = make.button(2)
+# left    = make.largemotor(7)
+# right   = make.largemotor(8)
+# robot   = make.drivetrain(left, right)
+# arm     = make.smallmotor(6)
+#
+# robot.drive(50, 2)
+# robot.turn(75, 1)
+# robot.drive(50, 2)
+#
+# arm.spin(100, 1.7)
+# arm.spin(-100, 1.7)
+#
+# robot.turn(25)
 # make.wait_until(stop.pressed)
+#
+# arm.spin(50)
+# robot.drive(50)
+# make.wait(10)
 # arm.stop()
+#
+# arm.spin(50, 2)
+#
+# # while not stop.pressed():
+# #     arm.spin(50, 0.05)
+#
+# # arm.spin(50)
+# # make.wait_until(stop.pressed)
+# # arm.stop()
+#
+# # motor demonstration
 
-# motor demonstration
+import make
+
+stop = make.button(port=2)
+left = make.largemotor(port=7, direction=1)
+right = make.largemotor(port=8, direction=1)
+arm = make.smallmotor(port=6, direction=-1)
+
+make.wait(seconds=2)
+if stop.pressed():
+  left.spin(speed=100)
+  right.spin(speed=100)
+  make.wait(seconds=2)
+  left.stop()
+  right.stop()
+else:
+  left.spin(speed=100)
+  right.spin(speed=100)
+  while not (stop.pressed()):
+    pass
+  left.stop()
+  right.stop()
