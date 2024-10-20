cc47f3693fc57ca0b464f6cad666883359c6715e
NullnoVoid <riveragabriel303@gmail.com>
2024-10-20 12:25:33 -0500

    Update distanceSensor.py

--- src/lib/proto/sensors/distanceSensor.py
+++ src/lib/proto/sensors/distanceSensor.py
@@ -1,12 +1,57 @@
 import board
 import digitalio
+import neopixel
+import simpleio
 import time
+import pwmio
+from adafruit_motor import servo, motor
 
-led = digitalio.DigitalInOut(board.GP2)
-led.direction = digitalio.Direction.OUTPUT
+# Initialize LEDs
+# LEDs placement on Maker Pi RP2040
+LED_PINS = [board.GP0,
+            board.GP1,
+            board.GP2,
+            board.GP3,
+            board.GP4,
+            board.GP5,
+            board.GP6,
+            board.GP7,
+            board.GP16,
+            board.GP17,
+            board.GP26,
+            board.GP27,
+            board.GP28]
 
-while True:
-            led.value = True
-            time.sleep(0.5)
-            led.value = False
-            time.sleep(0.5)
+LEDS = []
+for pin in LED_PINS:
+    # Set pins as digital output
+    digout = digitalio.DigitalInOut(pin)
+    digout.direction = digitalio.Direction.OUTPUT
+    LEDS.append(digout)
+
+
+
+
+
+# -------------------------------------------------
+# ON START: Show running light and play melody
+# -------------------------------------------------
+for i in range(len(LEDS)):
+    LEDS[i].value = True
+
+    if i < len(MELODY_NOTE):
+        # Play melody tones
+        simpleio.tone(PIEZO_PIN, MELODY_NOTE[i], duration=MELODY_DURATION[i])
+    else:
+        # Light up the remainding LEDs
+        time.sleep(0.15)
+
+# Turn off LEDs one-by-one very quickly
+for i in range(len(LEDS)):
+    LEDS[i].value = False
+    time.sleep(0.02)
+
+
+color = 0
+state = 0
+
