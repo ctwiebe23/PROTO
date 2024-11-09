e3d3d9d861f7b7840019d163c4593dc2466ef4e0
C Wiebe <carstonwiebe17@gmail.com>
2024-11-08 23:11:40 -0600

    redid demo program

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
-import lib.make as make
+import lib.make as make  # same as `import make`
 
-button = make.button( port=9 )
-motor = make.largemotor( port=6 )
+'''
+FOR:  PROTO Focus Group
+BY:   Carston Wiebe
+ON:   2024.11.08
+DOES: Endless loop where both buttons correspond to a largemotor.  When
+      a button is pressed, its paired motor spins uninterrupted for 5
+      seconds.  It then continues looping.
+'''
 
-if button.pressed():
-  motor.spin( power=100 )
-  make.wait( seconds=1 )
-  while not button.pressed():
-    make.wait()
-else:
-  motor.spin( power=100, seconds=5 )
+# name components
+motor1  = make.largemotor( port=6 )
+motor2  = make.largemotor( port=7 )
+button1 = make.button    ( port=8 )
+button2 = make.button    ( port=9 )
+
+while True:  # loops forever, because `True` is always true
+  
+    make.wait()  # wait for a very short time to stop the brain from
+                 # working as fast as it possibly can and overworking
+    
+    if button1.pressed():
+        motor1.spin( power=100, seconds=5 )
+    elif button2.pressed():  # 'elif' means 'else if'
+        motor2.spin( power=100, seconds=5 )
