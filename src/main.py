416c58e802f48db5cc4c4b49596aa7c51171e3c2
C Wiebe <carstonwiebe17@gmail.com>
2024-12-02 17:32:02 -0600

    documented main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 import lib.make as make
 
-left    = make.smallmotor(11)
-right   = make.smallmotor(13, -1)
+# name the drive motors
+left    = make.smallmotor( port=11 )
+right   = make.smallmotor( port=13, direction=-1 )  # this motor's facing the
+                                                    #  opposite direction
+# name the robot, using the motors
+robot   = make.drivetrain( left_motor=left, right_motor=right )
 
-robot   = make.drivetrain(left, right)
+# name the buttons
+button1 = make.button( port=8 )
+button2 = make.button( port=9 )
 
-button1 = make.button(8)
-button2 = make.button(9)
-
+# make actions using your robot
 while True:
-        robot.drive(100)
-        button2.wait_until_pressed()
-        robot.stop()
-        button2.wait_until_pressed()
+    robot.drive( power=100 )        # no time given -- spins forever!
+    button2.wait_until_pressed()    # motor still spins while the bot's waiting
+    robot.stop()                    # stops the motor no matter what
+    button2.wait_until_pressed()    # stays stopped while the bot's waiting
+    # loops back to the `while True`
