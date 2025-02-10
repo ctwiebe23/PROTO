5d14aa10bb00e6ea4a98111e349a3ad727876b99
C Wiebe <carstonwiebe17@gmail.com>
2025-02-10 15:03:29 -0600

    changed cservo range

--- src/lib/proto/schemata/cservo_schema.py
+++ src/lib/proto/schemata/cservo_schema.py
@@ -5,12 +5,12 @@ class cservo_schema:
     A continuous servo schema that details the motor frequency and a power
     scaler that takes one argument -- power -- and scales it to a valid
     throttle.
-    
+
     The power scaler should intake a power in the range [-100,100] and return
     a throttle in the range [-1,1], along with whatever other modifications
     are necessary.
     """
-    
+
     def __init__( self, frequency: int, power_scaler ):
         self.frequency      = frequency
         self.power_scaler   = power_scaler
@@ -25,6 +25,6 @@ FS90R: cservo_schema = cservo_schema(
         power,
         # servos have an asymmetric throttle range; they spin faster forwards
         # than backwards.  Slow down when driving forwards to compensate
-        (0.1,0.8) if power > 0 else (0.3,1),
+        0.8 if power > 0 else -1,
     ),
 )
