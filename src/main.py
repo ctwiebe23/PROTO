8c065e1845506d66843e05b0ff544acd4aa42748
C Wiebe <carstonwiebe17@gmail.com>
2025-03-30 16:44:29 -0500

    tractor drive code

--- src/main.py
+++ src/main.py
@@ -1,51 +1,12 @@
 from lib.make import *
 
-arm = smallmotor(1)
-hand = servo(2)
+drive_motor   = largemotor(7)
+start_button  = button(8)
+stop_button   = button(9)
 
-left = largemotor(6)
-right = largemotor(7)
+drive_motor.spin(100, 3)
 
-start_button = button(8)
-stop_button = button(9)
-
-bot = drivetrain(left, right, 1/1)
-
-arm.spin(100)
-wait_until(stop_button.pressed)
-arm.stop()
-wait(1)
-arm.spin(-100, 2)
-
-hand.moveto(0)
-wait(2)
-hand.moveto(90)
-wait(2)
-hand.moveto(180)
-
-wait(2)
-
-hand.moveto(0, 2)
-hand.moveto(90, 2)
-hand.moveto(180, 2)
-
-wait_until(start_button.pressed)
-
-bot.turn(100)
-wait(1)
-bot.turn(-100, 1)
-bot.stop()
-
-wait(1)
-
-bot.drive(100, 2)
-bot.curve(60, -20, 4)
-bot.curve(40, 60, 2)
-
-wait_until(start_button.pressed)
-bot.drive(100)
-wait_until(stop_button.pressed)
-
-# activity ideas:
-#   coin pick-up
-#   minefield
+while not stop_button.pressed():
+    if start_button.pressed():
+        drive_motor.spin(100, 3)
+    wait()
