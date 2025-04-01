561b38801605dbc2a713071730d60d9a2278d459
C Wiebe <carstonwiebe17@gmail.com>
2025-03-31 21:51:57 -0500

    began pi pico techdoc

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from lib.make import *
 
-drive_motor   = largemotor(7)
+drive_motor   = largemotor(7, -1)
 start_button  = button(8)
 stop_button   = button(9)
 
-drive_motor.spin(100, 3)
+drive_motor.spin(50, 2)
 
 while not stop_button.pressed():
     if start_button.pressed():
-        drive_motor.spin(100, 3)
+        drive_motor.spin(50, 2)
     wait()
