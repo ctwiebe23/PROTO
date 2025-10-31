e8b5984e485c2b8852d9db7c0697460fe3e45112
C Wiebe <carstonwiebe17@gmail.com>
2025-10-31 16:41:23 -0500

    changed default cservo stop value

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from lib.make import *
+import lib.make as make
 
-left = largemotor(6)
-right = largemotor(7, direction=-1)
-bot = drivetrain(left, right)
+base_motor = make.servo(1)
+pulley_motor = make.largemotor(7)
 
-stop = button(9)
+# Rotate crane to 80 degrees
+base_motor.moveto(80, 1)
 
-bot.drive(100)
-wait_until(stop.pressed)
-bot.stop()
+# Lower pulley and raise it back up
+pulley_motor.spin(100, 2)
+pulley_motor.spin_back(100, 2)
