5fa8929c767c667f372dd48fc5bbf0622521b993
C Wiebe <carstonwiebe17@gmail.com>
2026-04-04 15:25:16 -0500

    testing n20s with demo

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ import lib.make as make
 
 led1 = make.light(8)
 led2 = make.light(9)
-dc = make.drive_motor(4)
+dc = make.drive_motor(6)
 
-led2.on()
+led1.on()
 dc.spin(100)
 
 while True:
-  led1.toggle()
-  make.wait(0.1)
+  led2.toggle()
+  make.wait(0.2)
